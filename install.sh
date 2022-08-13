ac76193754ed952ab5f6dc073a4704648e1bb337
Martynas Sklizmantas <saint@ghost.lt>
2022-08-13 22:45:16 +0300

    third try

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 apt update
 apt install -y apt-transport-https
 
-wget --quiet --output-document - https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+sudo -u amnesia wget --quiet --output-document - https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+echo "deb [arch=amd64] tor+https://updates.signal.org/desktop/apt xenial main" | sudo tee /etc/apt/sources.list.d/signal-xenial.list
 
 
 apt update
 apt install -y signal-desktop
 
-echo "#!/bin/bash" > /usr/local/bin/signal-desktop
-echo "HTTPS_PROXY=socks5://127.0.0.1:9050 /opt/Signal/signal-desktop --no-sandbox" >> /usr/local/bin/signal-desktop
+echo "#!/bin/bash" | tee /usr/local/bin/run-signal
+echo "HTTPS_PROXY=socks5://127.0.0.1:9050 /opt/Signal/signal-desktop --no-sandbox" | tee -a /usr/local/bin/run-signal
 
-chmod +x /usr/local/bin/signal-desktop
+chmod +x /usr/local/bin/run-signal
 
 
 
